f65d41c2ef561fddc44d5d38aa83cd8f56dda0e9
xiaofei <lixiaofei0627@gmail.com>
2022-05-27 13:21:46 +0800

    feat: 优化缩放组件

--- src/components/ScaleBackground/index.vue
+++ src/components/ScaleBackground/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    ref="reference"
     class="scale-background"
     :style="{
       backgroundSize: `${backgroundSize}% auto`,
@@ -13,44 +14,59 @@
 </template>
 
 <script setup>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, ref, computed } from 'vue'
 import { onPageScroll } from '@dcloudio/uni-app'
 
+// 动画步长
 const step = 2
+// 顶部距离
+const top = 190
+// 当前页面元素
+const reference = ref()
 
 const state = reactive({
   backgroundSize: 100,
-  paddingTop: 181,
+  paddingTop: top,
   baseRate: 100,
   clientMoveY: 0,
-  pageY: 0,
+  scrollTop: 0,
   timer: null
 })
 
+// 监听页面滚动
 onPageScroll(e => {
-  state.pageY = e.scrollTop
-  console.log(state.pageY, ' state.pageY ')
+  state.scrollTop = e.scrollTop
 })
 
-const touchend = () => {
-  if (state.pageY !== 0) {
-    return
-  }
-  state.timer && clearInterval(state.timer)
-  // 回弹
-  state.timer = setInterval(() => {
-    state.baseRate = state.baseRate - step * 2
-    if (state.baseRate <= 100) {
-      state.baseRate = 100
-      state.paddingTop = 181
-      clearInterval(state.timer)
-    }
-    state.backgroundSize = state.baseRate
-    state.paddingTop = 181 * (state.baseRate / 100)
-  }, 16)
+// 拿到页面父元素
+const parentElements = computed(() =>
+  Array.from([
+    reference.value?.parentElement,
+    reference.value?.parentElement?.parentElement,
+    reference.value?.parentElement?.parentElement?.parentElement
+  ])
+)
+
+//  开始时处理页面滚动
+const handleMove = () => {
+  parentElements.value.forEach(item => {
+    item && (item.style.height = '100vh')
+    item && (item.style.overflow = 'hidden')
+  })
 }
+
+//  结束时处理页面滚动
+const handleEnd = () => {
+  parentElements.value.forEach(item => {
+    item && (item.style.height = '')
+    item && (item.style.overflow = '')
+  })
+}
+
+// 监听移动
 const touchmove = e => {
-  if (state.pageY !== 0) {
+  // 页面不在顶部不调用
+  if (state.scrollTop !== 0) {
     return
   }
   const touchData = e.changedTouches[0]
@@ -59,9 +75,31 @@ const touchmove = e => {
   if (state.clientMoveY - clientY < 0) {
     state.baseRate = state.baseRate + step
     state.backgroundSize = state.baseRate
-    state.paddingTop = 181 * (state.baseRate / 100)
+    state.paddingTop = top * (state.baseRate / 100)
   }
   state.clientMoveY = clientY
+  handleMove()
+}
+
+// 监听移动结束
+const touchend = () => {
+  // 页面不在顶部不调用
+  if (state.scrollTop !== 0) {
+    return
+  }
+  state.timer && clearInterval(state.timer)
+  // 回弹
+  state.timer = setInterval(() => {
+    state.baseRate = state.baseRate - step * 2
+    if (state.baseRate <= 100) {
+      state.baseRate = 100
+      state.paddingTop = top
+      clearInterval(state.timer)
+    }
+    state.backgroundSize = state.baseRate
+    state.paddingTop = top * (state.baseRate / 100)
+  }, 16)
+  handleEnd()
 }
 
 const { backgroundSize, paddingTop } = toRefs(state)
@@ -70,7 +108,6 @@ const { backgroundSize, paddingTop } = toRefs(state)
 <style lang="scss" scoped>
 .scale-background {
   background-repeat: no-repeat;
-  background-origin: center center;
   background-position: top center;
   background-size: 100% auto;
 }
